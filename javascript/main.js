f5b0969511b2a1de5ee08f3bd934f4cd96cf90ec
Aarón López <n_aaron5@hotmail.com>
2022-02-03 11:59:35 +0100

    canvas y start game function

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,26 @@
 // * GLOBAL VARIABLES
-
-
-
+let splashScreen = document.querySelector("#splash-screen");
+let canvas = document.querySelector("#my-canvas");
+let ctx = canvas.getContext("2d");
 
 
 // * STATE MANAGEMENT FUNCTIONS
+const startGame = () => {
+    // desaparecer el splash screen y aparecer canvas
+    splashScreen.style.display = "none";
+    canvas.style.display = "flex";
 
+    // ejecutar mi juego
+    
+};
 
 
 
 
 
 // * ADD EVENT LISTENERS
-
+let startButton = document.querySelector("#start-btn");
+startButton.addEventListener("click", startGame);
 
 
 
